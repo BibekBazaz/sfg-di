a5c0b88f77ddde331062f41a48c79d7a25e11147
BIBEK <bibekkumarbazaz@gmail.com>
2021-05-30 14:40:49 +0530

    Fixed the PetController class

--- src/main/java/guru/springframework/sfgdi/controllers/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -1,13 +1,25 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.PetService;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by jt on 12/28/19.
  */
+
+/*
+Fixes done:
+- added the @Service annotation, so Spring context can load the bean
+- added the constructor to autowire the PetService Bean
+ */
+@Service
 public class PetController {
 
     private final PetService petService;
+
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
 
     public String whichPetIsTheBest(){
         return petService.getPetType();
